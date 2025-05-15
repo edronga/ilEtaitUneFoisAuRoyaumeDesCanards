926cca3ee57f9c0e94f8c107c7374f76f7c0aebc
edronga <yanischess@gmail.com>
2025-05-15 18:55:13 +0200

    Add files via upload

--- eventListData/eventListData_Elevator.js
+++ eventListData/eventListData_Elevator.js
@@ -1,3 +1,26 @@
 'use strict'
 
 eventListElevator.add('goToHospitalHall', checkIfPosition(2, 6), ()=> {teleport('mapHospitalHall',19, 4 )})
+
+eventListElevator.addNpc('ghost', nonPlayableCharacters.ghostLookingDown, 4,2)
+
+bName = 'smoothGetOutOfElevator'
+bCondition = function(){
+    return (CACHE.targetTile.y >= 6)? true : false;
+}
+bAction = function (){
+    CACHE.targetTile = {x: 2, y:6}
+}
+eventListElevator.add(bName, bCondition, bAction)
+
+bName = 'makeGhostDisapear'
+bCondition = function (){
+    if (checkIfPosition(3,3)() || checkIfPosition(3,4)() || checkIfPosition(4,3)() || checkIfPosition(4,4)()){
+        return true
+    }
+    return false
+}
+bAction = function (){
+    eventListElevator.removeNpc('ghost')
+}
+eventListElevator.add(bName, bCondition, bAction)
